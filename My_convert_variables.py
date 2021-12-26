1bfe3dd347db7fcc764cf3f7fdac26d8eef93805
Kwadjo Edouwossi <kwadjooe@gmail.com>
2021-12-25 19:22:49 -0500

    very first commit

---
+++ My_convert_variables.py
@@ -0,0 +1,13 @@
+your_age = input("what is your age: ")
+
+your_age_as_number = int(your_age)
+
+your_age_as_number += 10
+
+print(f"your age + 10 is {your_age_as_number} ")
+
+# converting age into string
+
+your_age_as_text = str(your_age_as_number)
+
+print("your age is 10 " + your_age_as_text)
