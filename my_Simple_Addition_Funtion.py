1c2c2c7e169c83fc8ef6d8b7e1724ddf0609a5d4
Kwadjo Edouwossi <kwadjooe@gmail.com>
2022-01-04 18:49:12 -0500

    Adding a simple function for doing basic addition

---
+++ my_Simple_Addition_Funtion.py
@@ -0,0 +1,8 @@
+def add_numbers():
+    num_a = int(input("Please Provide a first Number: "))
+    num_b = int(input("Please Provide a second Number: "))
+
+    result = num_a + num_b
+    print(f"The sum of {num_a} and {num_b} is {result}")
+
+add_numbers()
