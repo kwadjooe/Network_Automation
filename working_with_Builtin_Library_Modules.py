8b5898c8c5efd0c1f2084a41cacfae9ba71240ff
Kwadjo Edouwossi <kwadjooe@gmail.com>
2022-01-06 18:57:36 -0500

    Adding a function to Generate password based on the random and string built-in libary Module

---
+++ working_with_Builtin_Library_Modules.py
@@ -0,0 +1,26 @@
+import random
+import string
+
+num_upper = random.randint(1, 9)
+print(f"Adding {num_upper} uppercase letters to the password")
+
+num_lower = random.randint(1, 9)
+print(f"Adding {num_lower} lowercase letters to the password")
+
+num_digits = random.randint(1, 9)
+print(f"Adding {num_digits} digits to the password")
+
+# defining a function to generate a password base on our variables
+def add_to_password(num_to_include, choices):
+    temp = ""
+    for i in range(num_to_include):
+        temp = temp + str(random.choice(choices))
+    return temp
+uppers = add_to_password(num_upper, string.ascii_uppercase)
+lowers = add_to_password(num_lower, string.ascii_lowercase)
+numbers = add_to_password(num_digits, string.digits)
+
+pwd = uppers + lowers + numbers
+
+print(f"The generated password is {pwd}")
+
