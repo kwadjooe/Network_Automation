d9f008415a6bdce68c6c5b0e5a85f1acab57f6f8
Kwadjo Edouwossi <kwadjooe@gmail.com>
2022-01-07 10:44:01 -0500

    Added a line to close Connection

--- ssh_Connection_with_Paramiko.py
+++ ssh_Connection_with_Paramiko.py
@@ -10,7 +10,13 @@ client = SSHClient()
 
 client.load_system_host_keys()
 try:
-    client.connect(SSH_HOST, port=SSH_PORT, username=SSH_USER, password=SSH_PASS, look_for_keys=False)
+    client.connect(SSH_HOST, port=SSH_PORT,
+                             username=SSH_USER,
+                             password=SSH_PASS,
+                             look_for_keys=False)
     print("Connection Successfull!")
 except Exception:
     print("Failed to Establish Connection. Please Try again.")
+
+finally:
+    client.close
