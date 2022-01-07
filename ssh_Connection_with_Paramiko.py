cb99c24aec0bf9262e6ee7141372786616aa2f52
Kwadjo Edouwossi <kwadjooe@gmail.com>
2022-01-07 12:46:39 -0500

    Added few changes to the script

--- ssh_Connection_with_Paramiko.py
+++ ssh_Connection_with_Paramiko.py
@@ -1,12 +1,15 @@
-from paramiko.client import SSHClient
+from paramiko.client import AutoAddPolicy, SSHClient
 import getpass
 
 SSH_USER = input("Please provide Username: ")
-SSH_PASS = getpass.getpass("Please provide Your Password: ")
+#SSH_PASS = getpass.getpass("Please provide Your Password: ") # or
+SSH_PASS = getpass.getpass(prompt="Please Provide your Password: ", stream=None)
 SSH_HOST = input("Please provide the Host IP Address: ")
 SSH_PORT = 22
 
 client = SSHClient()
+# this add new client host key not really a good idea for security reason
+client.set_missing_host_key_policy(AutoAddPolicy())
 
 client.load_system_host_keys()
 try:
